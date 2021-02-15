5d1c95e10766283c7a1806e6a10e591e327e9e1f
sahmadpur <sohrab.ahmadpur@gmail.com>
2021-02-15 21:15:50 +0400

    Done.

--- js/main.js
+++ js/main.js
@@ -271,15 +271,18 @@ jQuery(document).ready(function($) {
   var OnePageNavigation = function() {
     var navToggler = $('.site-menu-toggle');
    	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
-      e.preventDefault();
+    //   e.preventDefault();
 
       var hash = this.hash;
+	  if (hash != undefined) {
+		$('html, body').animate({
+			'scrollTop': $(hash).offset().top
+		  }, 600, 'easeInOutExpo', function(){
+			window.location.hash = hash;
+		  });
+	  } else{};
 
-      $('html, body').animate({
-        'scrollTop': $(hash).offset().top
-      }, 600, 'easeInOutExpo', function(){
-        window.location.hash = hash;
-      });
+      
 
     });
   };
